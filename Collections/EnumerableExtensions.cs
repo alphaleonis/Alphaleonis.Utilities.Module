b120f7636cacf578807031b908cb5ac8fa3738fb
Alphaleonis <alphaleonis@users.noreply.github.com>
2016-11-07 20:49:03 +0100

    More cleanup.

---
+++ Collections/EnumerableExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alphaleonis.Utilities
+{
+   public static class EnumerableExtensions
+   {
+      public static bool HasDuplicates<T>(this IEnumerable<T> self)
+      {
+         return HasDuplicates<T>(self, null);
+      }
+
+      public static bool HasDuplicates<T>(this IEnumerable<T> self, IEqualityComparer<T> comparer)
+      {
+         if (self == null)
+            throw new ArgumentNullException(nameof(self), $"{nameof(self)} is null.");
+
+         if (comparer == null)
+            comparer = EqualityComparer<T>.Default;
+
+         return self.GroupBy(x => x, comparer).Any(g => g.Count() > 1);
+      }
+   }
+}
